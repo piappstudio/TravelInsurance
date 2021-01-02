5017cb0faa46e99b0043002433fb0f17fde6f698
Pi App Studio <70383910+piappstudio@users.noreply.github.com>
2021-01-02 00:34:49 -0600

    Init commit
    Initial commit

---
+++ app/src/main/java/com/piappstudio/travelinsurance/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.piappstudio.travelinsurance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
