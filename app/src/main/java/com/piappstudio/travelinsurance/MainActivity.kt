2c4ca3c978a1fbc7ffbbb3f0ba85a53e3146f59e
Pi App Studio <70383910+piappstudio@users.noreply.github.com>
2021-06-04 22:35:05 -0500

    VehicleDetails Add & Update feature

--- app/src/main/java/com/piappstudio/travelinsurance/MainActivity.kt
+++ app/src/main/java/com/piappstudio/travelinsurance/MainActivity.kt
@@ -5,23 +5,18 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.piappstudio.pilibrary.ui.PIBaseActivity
-import com.piappstudio.pilibrary.utility.Logger
 import com.piappstudio.pilibrary.utility.dismissKeyboard
 import com.piappstudio.travelinsurance.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : PIBaseActivity() {
 
     private var binding:ActivityMainBinding? = null
 
-    @Inject lateinit var logger: Logger
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        logger.log("Muruga")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
         val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
